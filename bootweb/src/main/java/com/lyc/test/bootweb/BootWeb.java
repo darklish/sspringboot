cfb883138a8136ea250c0ca588a69a6e6c3b537b
limu <liyuanchao@liyuanchaodeMacBook-Pro.local>
2018-05-13 19:07:31 +0800

    ignore

--- bootweb/src/main/java/com/lyc/test/bootweb/BootWeb.java
+++ bootweb/src/main/java/com/lyc/test/bootweb/BootWeb.java
@@ -1,16 +1,19 @@
 package com.lyc.test.bootweb;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.servlet.DispatcherServlet;
 
 /**
  * Created by liyuanchao on 3/26/18.
  */
 @SpringBootApplication
+//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class BootWeb {
     public static void main(String[] args) {
-        DispatcherServlet servlet = new DispatcherServlet();
+        //DispatcherServlet servlet = new DispatcherServlet();
         //servlet.service();
         SpringApplication.run(BootWeb.class, args);
     }
